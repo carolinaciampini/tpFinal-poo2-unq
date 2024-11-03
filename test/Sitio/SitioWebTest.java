00e4c1dc414ad12cdcf6ce0ccc9bb201d5601a31
OrlandoAbril <orlandoabrillujan@gmail.com>
2024-11-03 20:33:45 -0300

    avances

--- test/Sitio/SitioWebTest.java
+++ test/Sitio/SitioWebTest.java
@@ -7,22 +7,28 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import Sitio.SitioWeb;
+import inmueble.Inmueble;
 import usuarios.Propietario;
+import usuarios.Usuario;
 
 class SitioWebTest {
 	private SitioWeb sitio;
 	private Usuario usuario1;
+	private Propietario propietario;
+	private Inmueble inmueble;
+	
 
 	@BeforeEach
 	void setUp() throws Exception {
-		sitio = new Sitio();
-		usuario = new Propietario("Abril", "abru@gmail.com", "1111111");
-		inmueble = new Inmueble ("Quinta", 123, "Argentina", "Hudson", "Calle 163 123", LocalTime.of(14,00), LocalTime.of(10,00), propietario);
+		sitio = new SitioWeb();
+		usuario1 = new Propietario("Abril", "abru@gmail.com", "1111111");
+		inmueble = new Inmueble ("Quinta", (double) 123, "Argentina", "Hudson", "Calle 163 123", LocalTime.of(14,00), LocalTime.of(10,00), propietario);
 	}
 
 	@Test
 	void testCantidadUsuarios() {
-		sitio.addUsuario(usuario);
+		sitio.addUsuario(usuario1);
 		assertEquals(sitio.getUsuarios().size(), 1);
 	}
 	
