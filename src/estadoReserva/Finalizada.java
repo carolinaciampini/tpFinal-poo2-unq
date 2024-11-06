63b7155585d1b11b01fb84dec7ee252721add60f
OrlandoAbril <orlandoabrillujan@gmail.com>
2024-11-06 13:46:36 -0300

    un cambio mas

--- src/estadoReserva/Finalizada.java
+++ src/estadoReserva/Finalizada.java
@@ -1,5 +1,9 @@
 package estadoReserva;
 
+import reserva.Reserva;
+import usuarios.Inquilino;
+import usuarios.Propietario;
+
 public class Finalizada extends EstadoReserva {
 	
 	public void realizarRanking(Reserva reserva, Inquilino inquilino, Propietario propietario) {
